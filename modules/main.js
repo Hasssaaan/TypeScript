13107ef9a53d6aeafd6a970c991e55e8b0b72638
Muhammad Hassaan <jahangirhassan78@gmail.com>
2024-03-16 16:00:05 +0500

    first commit

---
+++ modules/main.js
@@ -0,0 +1,6 @@
+import slogin from "./student.js";
+import tlogin from "./teacher.js";
+let student = new slogin();
+let teacher = new tlogin();
+console.log(teacher.data);
+console.log(student.data);
