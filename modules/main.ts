90ca3003addbb1da05f736c5cb16a2d8731f4eab
Hassaan <jahangirhassan78@gmail.com>
2024-04-18 10:12:40 +0500

    Testing

--- modules/main.ts
+++
@@ -1,6 +0,0 @@
-import slogin from"./student.js";
-import tlogin from "./teacher.js";
-let student = new slogin();
-let teacher = new tlogin();
-console.log(teacher.data);
-console.log(student.data);
--- modules/student.ts
+++
@@ -1,3 +0,0 @@
-export default class Login{
-    data="Student login done";
-}
